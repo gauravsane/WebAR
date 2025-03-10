c43173a4a128863e0c252c07e63eb363bee12a9d
gauravsane <sanegaurav360@gmail.com>
2025-03-10 15:45:34 +0530

    Modified

--- src/components/ObjectRender.jsx
+++ src/components/ObjectRender.jsx
@@ -15,11 +15,27 @@ const ObjectRender = () => {
     <div>
       <a-scene
         embedded
-        arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix;"
+        arjs="debugUIEnabled: true"
+        // arjs="sourceType: webcam; detectionMode: mono_and_matrix; matrixCodeType: 3x3; debugUIEnabled: true"
+        vr-mode-ui="enabled: false"
+        renderer="logarithmicDepthBuffer: true; precision: medium"
       >
+        {/* Fix for Material Errors */}
+        <a-assets>
+          <a-asset-item id="myModel" src="./3d-model.glb"></a-asset-item>
+        </a-assets>
+
         <a-marker preset="hiro">
-          {/* 3D Object - Red Box */}
+          {/* 3D Object */}
           <a-box position="0 0.5 0" material="color: red"></a-box>
+
+          {/* If using a GLB/GLTF model */}
+          <a-entity
+            gltf-model="#myModel"
+            scale="0.5 0.5 0.5"
+            position="0 0 0"
+            animation-mixer
+          ></a-entity>
         </a-marker>
 
         <a-entity camera></a-entity>

ba4691f679e38fa1c9feb48b8a97e897c6044621
gauravsane <sanegaurav360@gmail.com>
2025-03-10 15:32:49 +0530

    New Change

--- src/components/ObjectRender.jsx
+++ src/components/ObjectRender.jsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import "aframe";
 
 const ObjectRender = () => {
   useEffect(() => {
-    // Ensure A-Frame loads properly
-    const loadAR = async () => {
-      if (!window.AFRAME) {
-        console.error("A-Frame is not loaded");
+    // Load AR.js dynamically
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/gh/jeromeetienne/ar.js/aframe/build/aframe-ar.min.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Ensure camera permissions
+    if (navigator.mediaDevices?.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(() => console.log("Camera access granted"))
+        .catch((err) => console.error("Camera access denied:", err));
+    } else {
+      console.error("getUserMedia is not supported in this browser.");
+    }
+
+    return () => {
+      document.body.removeChild(script);
+
+      // Cleanup A-Frame scene on unmount
+      const scene = document.querySelector("a-scene");
+      if (scene) {
+        scene.parentNode.removeChild(scene);
       }
     };
-    loadAR();
   }, []);
+
   return (
     <div>
-    <a-scene mindar-image="imageTargetSrc: ./marker.mind;" embedded arjs>
-      <a-camera position="0 0 0" look-controls="false"></a-camera>
+      <a-scene
+        embedded
+        arjs="sourceType: webcam; debugUIEnabled: false"
+      >
+        <a-camera position="0 0 0"></a-camera>
 
-      {/* AR Marker */}
-      <a-marker preset="hiro">
-        {/* 3D Object */}
-        <a-box position="0 0.5 0" material="color: red"></a-box>
-      </a-marker>
-    </a-scene>
-  </div>
-  )
-}
+        {/* AR Marker */}
+        <a-marker preset="hiro">
+          {/* 3D Object */}
+          <a-box position="0 0.5 0" material="color: red"></a-box>
+        </a-marker>
+      </a-scene>
+    </div>
+  );
+};
 
-export default ObjectRender
+export default ObjectRender;
